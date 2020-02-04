c7817fbb7701a9a01f782392be6efbb414b01d1e
Gagan <gaganjot.singh.randhawa@sap.com>
2020-02-05 00:32:07 +0530

    movie component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
-import '../App.css';
-import Header from './Header'
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
-function App() {
+//api key e3b27be2
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=e3b27be2"; // you should replace this with yours
+
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=e3b27be2`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
+
+
   return (
-    <div className="App-header">
+    <div >
       <Header text="Movie Search Application" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+              movies.map((movie, index) => (
+                <Movie key={`${index}-${movie.Title}`} movie={movie} />
+              ))
+            )}
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
