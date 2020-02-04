52e4015e9606771810b40ca99fbe1974f561bc32
Gagan <gaganjot.singh.randhawa@sap.com>
2020-02-04 22:34:22 +0530

    Updates

---
+++ precache-manifest.9c7c15581532fe287957df76fb5c3691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bbc2fc9c53946afdc150c82a8d44c0",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7a454817da89a12b9a52",
+    "url": "/movie-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9c66b7e6cdaaff472896",
+    "url": "/movie-app/static/js/2.42c508ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-app/static/js/2.42c508ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a454817da89a12b9a52",
+    "url": "/movie-app/static/js/main.7bf4d8e2.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
