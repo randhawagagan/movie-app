8a85c10c9759d7b48b884d3e9ae728685478c5bc
Gagan <gaganjot.singh.randhawa@sap.com>
2020-02-04 23:54:48 +0530

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import logo from '../disney.svg';
+import '../App.css'
 const Header = (props) => {
   return (
     <header className="App-header">
-      <h2>{props.text}</h2>
+      <img src={logo} alt="logo" />
+      <h2 className="headertext">{props.text}</h2>
     </header>
   );
 };
